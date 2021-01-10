aedbc40fa74e2b46ef32a5fcf0518325d2c7c721
Vladyslav <lefttwixwand@gmail.com>
2021-01-10 21:42:45 +0200

    Added Amadeus api library

--- AirTickets/Views/MainPage.xaml.cs
+++ AirTickets/Views/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Migrations;
+﻿using amadeus;
+using amadeus.util;
+using Migrations;
 using Migrations.Models;
 using System;
 using Windows.UI.Xaml;
@@ -18,13 +20,50 @@ namespace AirTickets.Views
             this.InitializeComponent();
         }
 
+        public static void GetLocations()
+        {
+            try
+            {
+
+                var apikey = "REPLACE_BY_YOUR_API_KEY";
+                var apisecret = "REPLACE_BY_YOUR_API_SECRET";
+
+                Configuration amadeusconfig = Amadeus.builder(apikey, apisecret);
+                amadeusconfig.setLoglevel("debug");
+                Amadeus amadeus = amadeusconfig.build();
+
+                Console.WriteLine("Get CheckinLink:");
+                amadeus.resources.CheckinLink[] checkinLinks = amadeus.referenceData.urls.checkinLinks.get(Params
+                        .with("airlineCode", "BA"));
+
+                Console.WriteLine(AmadeusUtil.ArrayToStringGeneric(checkinLinks, "\n"));
+
+                Console.WriteLine("\n\n");
+
+                Console.WriteLine("Get Locations:");
+
+                amadeus.resources.Location[] locations = amadeus.referenceData.locations.get(Params
+                    .with("keyword", "LON")
+                    .and("subType", resources.referenceData.Locations.ANY));
+
+                Console.WriteLine(AmadeusUtil.ArrayToStringGeneric(locations, "\n"));
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR: " + e.ToString());
+            }
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            using (DatabaseContext context = new DatabaseContext())
-            {
-                context.Users.Add(new User() { ID = 0, Name = "USer1" });
-                context.SaveChanges();
-            }
+            //using (DatabaseContext context = new DatabaseContext())
+            //{
+            //    context.Users.Add(new User() { ID = 0, Name = "USer1" });
+            //    context.SaveChanges();
+            //}
+
+            GetLocations();
         }
     }
 }
